8e39384feb44b673fb932c6eb53256b806bef595
crisefd <ophiuco93@hotmail.com>
2015-05-25 08:04:35 -0500

    Implementacion de modelo 2 con error

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -35,21 +35,123 @@ except IOError as err:
 	print err
 	path = os.path.abspath(os.path.dirname(__file__)  + '.././Logic/solver.py')
 	solver = imp.load_source("solver", path)
+"""
+class Frame(QtGui.QFrame):
+	def setupUi(self, Frame):
+		print type(Frame)
+		Frame.setObjectName(_fromUtf8("Frame"))
+		Frame.resize(266, 179)
+       	Frame.setFrameShape(QtGui.QFrame.StyledPanel)
+       	Frame.setFrameShadow(QtGui.QFrame.Raised)
+       	self.buttonOne = QtGui.QPushButton(Frame)
+       	self.buttonOne.setGeometry(QtCore.QRect(80, 50, 100, 27))
+       	self.buttonOne.setObjectName(_fromUtf8("buttonOne"))
+       	self.buttonTwo = QtGui.QPushButton(Frame)
+       	self.buttonTwo.setGeometry(QtCore.QRect(80, 100, 100, 27))
+       	self.buttonTwo.setObjectName(_fromUtf8("buttonTwo"))
+       	self.retranslateUi(Frame)
+       	QtCore.QObject.connect(self.buttonOne, QtCore.SIGNAL(_fromUtf8("pressed()")), Frame.solveProblemOne)
+       	QtCore.QObject.connect(self.buttonTwo, QtCore.SIGNAL(_fromUtf8("pressed()")), Frame.solveProblemTwo)
+       	QtCore.QMetaObject.connectSlotsByName(Frame)
 
-class AppGUI(QtGui.QFrame):
-	def __init__(self):
-		super(AppGUI, self).__init__()
-		self.solver = solver.Solver1()
+
+	def solveProblemOne(self):
+		solver = solver.Solver1()
 		path = QtGui.QFileDialog.getOpenFileName(None,
    			 _fromUtf8("Open File"), "/home", _fromUtf8("Files (*.txt *.ini)"))
-		self.solver.initialize(path)
-		data = self.solver.solve()
+		solver.initialize(path)
+		data = solver.solve()
 		str_data = "Configuration:\n"+ data['msg'] + "\n" + "Solution: "+ data['indices_solution'] + "\n" + "Solution Value: "+ data['solution_value'] + "\n" +"Iterations: "+ data['Iterations']
 		msgBox = QtGui.QMessageBox.information(self, _fromUtf8("Salida"),_fromUtf8(str_data), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
-		#self.close()
+		self.close()
+
+	def solveProblemTwo(self):
+		solver = solver.Solver1()
+		path = QtGui.QFileDialog.getOpenFileName(None,
+   			 _fromUtf8("Open File"), "/home", _fromUtf8("Files (*.txt *.ini)"))
+		solver.initialize(path)
+		data = solver.solve()
+		M = data['solution_value']
+		P = solver.P
+		p = solver.p
+		V = solver.V
+		v = solver.v
+		solver = solver.Solver2()
+		solver.initialize(M, p, v, P, V)
+		data = solver.solve()
+		str_data = "Configuration:\n"+ data['msg'] + "\n" + "Solution: "+ data['indices_solution'] + "\n" + "Solution Value: "+ data['solution_value'] + "\n" +"Iterations: "+ data['Iterations']
+		msgBox = QtGui.QMessageBox.information(self, _fromUtf8("Salida"),_fromUtf8(str_data), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+		self.close()
+
+	def retranslateUi(self, Frame):
+		Frame.setWindowTitle(_translate("Frame", "Frame", None))
+       	self.buttonOne.setText(_translate("Frame", "Problem One", None))
+       	self.buttonTwo.setText(_translate("Frame", "Problem Two", None))
+
+	def __init__(self):
+		print "Inicializando"
+		super(Frame, self).__init__()
+		#print type(Frame)
+		self.setupUi(self)
+"""
+
+class Frame(QtGui.QFrame):
+    def __init__(self):
+        super(Frame, self).__init__()
+        self.setupUi(self)
+
+    def setupUi(self, Frame):
+        Frame.setObjectName(_fromUtf8("Frame"))
+        Frame.resize(266, 179)
+        Frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        Frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.buttonOne = QtGui.QPushButton(Frame)
+        self.buttonOne.setGeometry(QtCore.QRect(80, 50, 100, 27))
+        self.buttonOne.setObjectName(_fromUtf8("buttonOne"))
+        self.buttonTwo = QtGui.QPushButton(Frame)
+        self.buttonTwo.setGeometry(QtCore.QRect(80, 100, 100, 27))
+        self.buttonTwo.setObjectName(_fromUtf8("buttonTwo"))
+
+        self.retranslateUi(Frame)
+        QtCore.QObject.connect(self.buttonOne, QtCore.SIGNAL(_fromUtf8("pressed()")), Frame.solveProblemOne)
+       	QtCore.QObject.connect(self.buttonTwo, QtCore.SIGNAL(_fromUtf8("pressed()")), Frame.solveProblemTwo)
+        QtCore.QMetaObject.connectSlotsByName(Frame)
+
+    def solveProblemOne(self):
+		solver_ = solver.Solver1()
+		path = QtGui.QFileDialog.getOpenFileName(None,
+   			 _fromUtf8("Open File"), "/home", _fromUtf8("Files (*.txt *.ini)"))
+		solver_.initialize(path)
+		data = solver_.solve()
+		str_data = "Configuration:\n"+ data['msg'] + "\n" + "Solution: "+ data['indices_solution'] + "\n" + "Solution Value: "+ data['solution_value'] + "\n" +"Iterations: "+ data['Iterations']
+		msgBox = QtGui.QMessageBox.information(self, _fromUtf8("Salida"),_fromUtf8(str_data), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+		self.close()
+
+    def solveProblemTwo(self):
+		solver_ = solver.Solver1()
+		path = QtGui.QFileDialog.getOpenFileName(None,
+   			 _fromUtf8("Open File"), "/home", _fromUtf8("Files (*.txt *.ini)"))
+		solver_.initialize(path)
+		data = solver_.solve()
+		M = int(float(data['solution_value']))
+		P = solver_.P
+		p = solver_.p
+		V = solver_.V
+		v = solver_.v
+		solver_ = solver.Solver2()
+		solver_.initialize(M, p, v, P, V)
+		data = solver_.solve()
+		str_data = "Configuration:\n"+ data['msg'] + "\n" + "Solution: "+ data['indices_solution'] + "\n" + "Solution Value: "+ data['solution_value'] + "\n" +"Iterations: "+ data['Iterations']
+		msgBox = QtGui.QMessageBox.information(self, _fromUtf8("Salida"),_fromUtf8(str_data), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
+		self.close()
+
+    def retranslateUi(self, Frame):
+        Frame.setWindowTitle(_translate("Frame", "Frame", None))
+        self.buttonOne.setText(_translate("Frame", "Problem One", None))
+        self.buttonTwo.setText(_translate("Frame", "Problem Two", None))
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    w = AppGUI()
-    #w.show()
+    w = Frame()
+    w.show()
     sys.exit(app.exec_())
--- Logic/solver.py
+++ Logic/solver.py
@@ -172,6 +172,46 @@ class Solver2(Solver):
 		self.P = P
 		self.V = V
 
+	def solve(self):
+		global lp
+		L = self.M * self.M + 2
+		print "L=", L
+		self._add_constraints()
+		self._set_objective()
+		lp.solve()
+		sol = lp.getSolution()
+		#print ("=========>", len(sol))
+		#sol_matrix = []
+		msg = ""
+		k = 0
+		p = self.M
+		for i in range(0, self.M):
+			#print "k=",k," p=",p
+			msg += "knapsack " + str(i) + " boxes: "
+			row = sol[k:p]
+			for j in range(0, len(row)):
+				if row[j] == 1:
+					msg += str(j) + ", "
+
+			print row
+			msg += "\n"
+			#sol_matrix.append(row)
+			k += self.M + 1
+			p += self.M + 1
+		#solution = ''.join(str(e) + "," for e in sol)
+		indices_solution = ''.join(str(e)+ ", " for e in lp.getSolution([L - 2, L - 1]))
+		solution_value = str(lp.getObjectiveValue())
+		iterations = str(lp.getInfo('Iterations'))
+		output = {'msg':msg,'indices_solution':indices_solution,
+					'solution_value':solution_value,'Iterations':iterations}
+		#print "=========================================="
+		#print "Solution: ", lp.getSolution()
+		#print "Solucion with indices: ", lp.getSolution(r)
+		#print "Solution value: ", lp.getObjectiveValue()
+		#print "Info: ", lp.getInfo('Iterations')
+		lp = None
+		return output
+
 	def _add_constraints(self):
 		global lp
 		#Copying weights and volumes
@@ -182,87 +222,147 @@ class Solver2(Solver):
 		#Setting the amount of the variables in the matrix
 		L = self.M * self.M + 2
 		indices = [i for i in range(0, L)]
-		#Setting the right hand side constants of the equations
-		rhs = []
-		for i in range(0, L):
-			if i == L - 1 or i == L - 2:
-				rhs.append(0)
-			elif i%2 == 0:
-				rhs.append(self.P)
-			else:
-				rhs.append(self.V)
+		lp.setBinary(indices)
+		print "Indices=", indices
+		#rhs = [0] * (2*self.M)
+		#print "rhs=", rhs
+		#for index in range(0, self.M):
+		#	rhs[index] = self.P
+		#for index in range(self.M, 2*self.M):
+		#	rhs[index] = self.V
+
+		print "==========Restriccion a)"
 		coefficients = []
 		k = 0
-		for i in range(0, 2*self.M):
+		for i in range(0, self.M):
+			n = 0
+			tuple_ = [0] * L
+			if i==0:
+				x = k
+				y = (i + 1) * (self.M)
+				k = k + self.M 
+			else:
+				x = k #<--------------------------------------
+				y = (i + 1) * (self.M)
+				k = k + self.M #<-------------------------------
+			print "x=", x
+			print "y=", y
+			for j in range(x, y):
+				try:
+					tuple_[j] = coeff_p[n]
+				except IndexError:
+					print "Error j=",j,"n=", n
+				n += 1
+			coefficients.append(tuple_)
+		print "coefficients=", coefficients
+		lp.addConstraint((indices, coefficients), "<=", [self.P for i in range(0, self.M)])
+
+
+		print "==========Restriccion b)"
+		coefficients = []
+		k = 0
+		for i in range(0, self.M):
+			n = 0
+			tuple_ = [0] * L
+			if i==0:
+				x = k
+				y = (i + 1) * (self.M)
+				k = k + self.M 
+			else:
+				x = k #<--------------------------------------
+				y = (i + 1) * (self.M)
+				k = k + self.M #<-------------------------------
+			print "x=", x
+			print "y=", y
+			for j in range(x, y):
+				try:
+					tuple_[j] = coeff_v[n]
+				except IndexError:
+					print "Error j=",j,"n=", n
+				n += 1
+			coefficients.append(tuple_)
+		print "coefficients=", coefficients
+		lp.addConstraint(coefficients, "<=", self.V)
+
+		print "==========Restriccion c)"
+		for i in range(0, self.M):
+			coefficients = [0] * L
+			z = i
+			while z < L - 2:
+				print "z=", z
+				coefficients[z] = 1
+				z += self.M
+			#print "Last constraint coeffs ", c
+			lp.addConstraint(coefficients, "=", 1)
+			print coefficients
+
+		
+		print "==========Restriccion d) "
+		k = 0
+		coefficients = []
+		for i in range(0, self.M):
 			n = 0
 			#Initializing a coefficient row to zeros
 			tuple_ = [0] * L
 			if i==0:
 				x = k
-				y = (i + 1) * (self.M + 1)
-				k = k + self.M 
+				y = (i + 1) * (self.M)
+				k = k + self.M
 			else:
-				x = k + 1
-				y = (i+1) * (self.M + 1)
-				k = k + self.M + 1
-			#print "x=", x
-			#print "y=", y
-			#Inserting v + p values in the row 
+				x = k 
+				y = (i + 1) * (self.M)
+				k = k + self.M
+			print "x=", x
+			print "y=", y
 			for j in range(x, y):
-				#print "j=",j,"n=",n
 				try:
-					if i < 2*self.M:
-						tuple_[j] = coeff_p[n]
-					else:
-						tuple_[j] = coeff_v[n]
+					tuple_[j] = coeff_p[n]
 				except IndexError:
 					print "Error j=",j,"n=", n
 				n += 1
+			tuple_[L - 2] = -1
 			#Appending new row to the coefficients
-			coefficients.append(tuple_1)
+			coefficients.append(tuple_)
+		print "coefficients=", coefficients
+		lp.addConstraint(coefficients, "<=", 0)
 
+		print "==========Restriccion e) "
 		k = 0
-		for i in range(0, 2*self.M):
+		coefficients = []
+		for i in range(0, self.M):
 			n = 0
 			#Initializing a coefficient row to zeros
-			tuple_2 = [0] * L
+			tuple_ = [0] * L
 			if i==0:
 				x = k
-				y = (i + 1) * (self.N + 1)
-				k = k + self.N 
+				y = (i + 1) * (self.M)
+				k = k + self.M
 			else:
-				x = k + 1
-				y = (i+1) * (self.N + 1)
-				k = k + self.N + 1
-			#print "x=", x
-			#print "y=", y
-			#Inserting v + p values in the row 
+				x = k
+				y = (i + 1) * (self.M)
+				k = k + self.M
+			print "x=", x
+			print "y=", y
 			for j in range(x, y):
-				#print "j=",j,"n=",n
 				try:
-					if i < 2*self.M:
-						tuple_2[j] = coeff_p[n]
-					else:
-						tuple_2[j] = coeff_p[n] * -1
+					tuple_[j] = coeff_p[n] * -1
 				except IndexError:
 					print "Error j=",j,"n=", n
 				n += 1
-
-			if i < 2*self.M:
-				tuple_2[j].append(-1)
-			else:
-				tuple_2[j].append(1)	
-
-
+			tuple_[L - 1] = 1
 			#Appending new row to the coefficients
-			coefficients.append(tuple_2)
-
-		lp.addConstraint((indices, coefficients), "<=", rhs)
-
+			coefficients.append(tuple_)
+		print "coefficients=", coefficients
+		lp.addConstraint(coefficients, "<=", 0)
 
 	def _set_objective(self):
 		global lp
-		L = self.M + len(self.p) + 2
+		L = self.M * self.M + 2
+		coefficients = [0] * L
+		coefficients[L - 2] = 1
+		coefficients[L - 1] = 1
+		print "==========Objective ", coefficients
+		lp.setObjective(coefficients, mode="minimize")
 
 
 
